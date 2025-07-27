ab7b6cf5862484e558c771fe740ce03a19690b5c
Saarthi09 <saarthibasantgupta31@gmail.com>
2025-07-27 18:16:42 +0530

    adding comments for self reference

--- project.py
+++ project.py
@@ -4,14 +4,14 @@ from PIL import Image
 from sklearn.decomposition import PCA
 import os
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+script_dir = os.path.dirname(os.path.abspath(__file__)) #this will set the path for the user
 user_input = input("Enter file name: ").strip()
 
 if not os.path.isabs(user_input):
-    filename = os.path.join(script_dir, user_input)
+    filename = os.path.join(script_dir, user_input) #in case the file isnt available 
 else:
     filename = user_input
 
-if not os.path.isfile(filename):
+if not os.path.isfile(filename): #these will close the program in case of file failure (failure of acquirin)
     print("File cannot be opened:", filename)
     quit()
