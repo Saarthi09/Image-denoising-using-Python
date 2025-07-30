41536e1916f52c54d33db91bde30860a114910ff
Saarthi09 <saarthibasantgupta31@gmail.com>
2025-07-30 09:19:47 +0530

    processing and applying PCA on input image

--- project.py
+++ project.py
@@ -15,3 +15,23 @@ else:
 if not os.path.isfile(filename): #these will close the program in case of file failure (failure of acquirin)
     print("File cannot be opened:", filename)
     quit()
+
+#now we will use the input image and do the work required
+#we will start by opening, reading and converting the image to grayscale
+image = Image.open(filename).convert('L')
+#now convert the image to array 
+img_array = np.asarray(image)
+
+#defining PCA
+def pca_denoise(image, num_components, axis=0):
+    if axis == 1:
+        image = image.T
+
+    pca = PCA(n_components=min(num_components, image.shape[1]))
+    transformed = pca.fit_transform(image)
+    reconstructed = pca.inverse_transform(transformed)
+    
+    if axis == 1:
+        reconstructed = reconstructed.T
+
+    return np.clip(reconstructed, 0, 255)
