b65030383810019f474df727e111d84dfaae661f
Saarthi09 <saarthibasantgupta31@gmail.com>
2025-07-31 12:20:01 +0530

    Final code: displaying denoised image

--- denoiser.py
+++ denoiser.py
@@ -4,7 +4,7 @@ from PIL import Image
 from sklearn.decomposition import PCA
 import os
 
-script_dir = os.path.dirname(os.path.abspath(__file__)) #this will set the path for the user
+script_dir = os.path.dirname(os.path.abspath(__file__)) #this will set the path for the user and the input file's location will be in reference to this path
 user_input = input("Enter file name: ").strip()
 
 if not os.path.isabs(user_input):
@@ -12,7 +12,7 @@ if not os.path.isabs(user_input):
 else:
     filename = user_input
 
-if not os.path.isfile(filename): #these will close the program in case of file failure (failure of acquirin)
+if not os.path.isfile(filename): #this will close the program in case of file failure (failure of acquirin)
     print("File cannot be opened:", filename)
     quit()
 
@@ -24,14 +24,33 @@ img_array = np.asarray(image)
 
 #defining PCA
 def pca_denoise(image, num_components, axis=0):
-    if axis == 1:
-        image = image.T
+    if axis == 1: #PCA now operates across coloumns instead of rows
+        image = image.T #Transpose
 
-    pca = PCA(n_components=min(num_components, image.shape[1]))
-    transformed = pca.fit_transform(image)
+    pca = PCA(n_components=min(num_components, image.shape[1])) #applying pca
+    transformed = pca.fit_transform(image) #merging both fit + transform 
     reconstructed = pca.inverse_transform(transformed)
     
     if axis == 1:
-        reconstructed = reconstructed.T
+        reconstructed = reconstructed.T #transposes back to normal 
 
-    return np.clip(reconstructed, 0, 255)
+    return np.clip(reconstructed, 0, 255) #makes sure that pixel values lie from 0 to 255 
+
+#now we will use our defined function
+denoised_img = pca_denoise(img_array, num_components=50)
+
+#the input image is now denoised so we will use matplotlib to display the changes
+#first we display the original image in grayscale
+plt.figure(figsize=(12,4))
+plt.subplot(1,2,1)
+plt.title("Before")
+plt.imshow(img_array, cmap='gray') #this will display in grayscale 
+plt.axis("off")
+
+plt.subplot(1,2,2)
+plt.title("After")
+plt.imshow(denoised_img, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
